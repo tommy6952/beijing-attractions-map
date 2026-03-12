e6aaa8d6fcce5f67cd267deda83a74ce076b46c4
tommy6952 <120072280@qq.com>
2026-03-12 11:16:57 +0800

    Add files via upload

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -50,11 +50,12 @@ export default function Map({ selectedAttraction, onSelectAttraction }) {
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          subdomains={['a', 'b', 'c', 'd']}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          subdomains={['a', 'b', 'c']}
           maxZoom={19}
           minZoom={3}
+          crossOrigin="anonymous"
         />
         
         <MapController selectedAttraction={selectedAttraction} />
